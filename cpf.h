49dbf277e381b4bdcc2797034ab78073eae0d09e
IvysonWNM <ivysonnunes@hotmail.com>
2024-11-30 11:27:30 -0300

    Adiciona Funções para Gerenciar Lista Dinâmica de CPFs

--- cpf.h
+++ cpf.h
@@ -1,1 +1,10 @@
+#ifndef CPF_H
+#define CPF_H
 
+// Função para adicionar um CPF à lista
+void adicionaCPF(const char *cpf);
+
+// Função para liberar a memória alocada para os CPFs
+void liberaMemoria();
+
+#endif // CPF_H
