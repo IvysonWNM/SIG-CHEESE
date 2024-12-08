d54fcf06e79f8d38e2d806477cbe0194788bc5a0
IvysonWNM <ivysonnunes@hotmail.com>
2024-12-08 19:13:16 -0300

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -1,13 +1,6 @@
 #ifndef CLIENTE_H  // Prevenção de múltiplas inclusões: garante que o arquivo só será incluído uma vez
 #define CLIENTE_H  // Marca CLIENTE_H como definido
 
-// Declaração das funções relacionadas ao cliente
-void menu_cliente(void);
-void cadastra_cliente(void);
-void pesquisa_cliente(void);
-void atualiza_cliente(void);
-void exclui_cliente(void);
-
 // Estrutura para armazenar os dados do cliente
 typedef struct {
     char status;      // Status do cliente (ativo, inativo, etc.)
@@ -17,5 +10,15 @@ typedef struct {
     char *data;       // Data de nascimento ou cadastro dinâmico
     char *fone;       // Telefone dinâmico
 } Cliente;
+
+// Declaração das funções relacionadas ao cliente
+void grava_cliente(FILE *fp, Cliente *cliente);
+void busca_cliente(const char *cpf_busca);
+
+void menu_cliente(void);
+void cadastra_cliente(void);
+void pesquisa_cliente(void);
+void atualiza_cliente(void);
+void exclui_cliente(void);
 
 #endif // Finaliza a proteção
